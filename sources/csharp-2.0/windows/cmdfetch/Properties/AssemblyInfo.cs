f9c8f4fd6337287716338f98c56d9e71aa821d4f
hotlandsoftware <46288609+hotlandsoftware@users.noreply.github.com>
2019-07-27 19:48:28 -0400

    Del obj folder

--- sources/csharp-2.0/windows/cmdfetch/Properties/AssemblyInfo.cs
+++ sources/csharp-2.0/windows/cmdfetch/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("cmdfetch")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Hotlands Software 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
